7ee9fe4d9be2100d9eb21e65de6b04370c584d2c
devclo <37320389+devclo@users.noreply.github.com>
2023-12-03 23:09:21 +0000

    streamlit

--- transcription_project/transcription_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Transcription
-
-class TranscriptionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Transcription
-        fields = '__all__'
