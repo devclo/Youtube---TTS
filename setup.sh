3f89b49dca0725c1ae463e689d965a0cbf6ce993
devclo <37320389+devclo@users.noreply.github.com>
2024-01-03 03:31:17 +0000

    setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,4 @@ python -m pip install --upgrade pip
 pip install streamlit yt-dlp ffmpeg-python moviepy srt torchaudio whisper
 
 # Run the main Python script
-python main.py
-
 streamlit run main.py
